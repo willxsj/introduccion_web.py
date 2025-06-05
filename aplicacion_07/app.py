4c819c869bb4f692c6455260ec673f01bf390495
willxsj <1724110309@utectulancingo.edu.mx>
2025-06-05 15:02:34 +0000

    CREATE calculadora

--- aplicacion_07/app.py
+++ aplicacion_07/app.py
@@ -13,25 +13,35 @@ app = web.application(urls, globals())
 class Index:
     def GET(self):
         return render.index() 
+    
     def POST(self):
-        print("Recibiendo datos del formulario")
-        formulario = web.input()
-        print(formulario)
+        try:
+                print("Recibiendo datos del formulario")
+                formulario = web.input()
+                print(formulario)
 
-        numero1 = int(formulario.numero1)
-        numero2 = int(formulario.numero2)
-        operacion = formulario.operacion
+       
+                numero1 = float(formulario.numero1)
+                numero2 = float(formulario.numero2)
+                operacion = formulario.operacion
 
-        if operacion == "sumar":
-            resultado = numero1 + numero2
-        elif operacion == "restar":
-            resultado = numero1 - numero2
-        elif operacion == "multiplicar":
-            resultado = numero1 * numero2
-        elif operacion == "dividir":
-            resultado = numero1 / numero2
-        else:
-            resultado = 0
+                if operacion == "sumar":
+                    resultado = numero1 + numero2
+                elif operacion == "restar":
+                    resultado = numero1 - numero2
+                elif operacion == "multiplicar":
+                    resultado = numero1 * numero2
+                elif operacion == "dividir":
+                    resultado = numero1 / numero2
+                else:
+                    resultado = "no valido"
+
+
+        except Exception as e:
+            print(f"Error 001:  {e.arg[0]}")
+            print("Error al dividir") 
+            
+
 
         print(f"Resultado: {resultado}")
         return render.index(resultado)
